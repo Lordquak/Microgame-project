91559daad048ef0f897f08d7983d0f17f72ff475
mkabulska <mkabulska2023@elam.co.uk>
2024-01-23 12:46:33 +0000

    gfd
    fcv

--- MGP Project - MLJ/Assets/Scripts/SceneManager.cs
+++ MGP Project - MLJ/Assets/Scripts/SceneManager.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Teleport : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKey(KeyCode.B))
         {
             SceneManager.LoadScene("Level 1");
         }
